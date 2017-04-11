60de2e5d9308d1bc614f780c934e94ffeaefc561
QDBA <dieter@baur.nu>
2017-04-11 13:33:28 +0200

    Add responder.deb

--- Payloads/extensions/debug.sh
+++ Payloads/extensions/debug.sh
@@ -10,29 +10,31 @@ function START_DEBUG() {
 	
 	tail -n 1 -f /var/log/syslog > $LOGFILE &
 	sleep 1
-	logger -t PAYLOAD "################ Start Debug ###################"
+	logger -t "[PAYLOAD]" "################ Start Debug ###################"
 }
 export -f START_DEBUG
 
 
 function STOP_DEBUG() {
-	logger -t PAYLOAD "################ Stop Debug ###################"
+	logger -t "[PAYLOAD]" "################ Stop Debug ###################"
 	killall tail
 	if [ "$(mount | grep /dev/nandf | awk '{printf $3}')" == "/root/udisk" ];then	
 		cp $LOGFILE /root/udisk/loot/log.txt
-		logger "copied $LOGFILE file to /root/udisk/loot/log.txt"
+		logger -t "[PAYLOAD]" "copied $LOGFILE file to /root/udisk/loot/log.txt"
+		sync; sleep 1; sync
 	else
-		mount /dev/nandf /root/udisk		
+		mount -o sync /dev/nandf /root/udisk		
 		sleep 1
 		cp $LOGFILE /root/udisk/loot/log.txt
-		logger "mount /dev/nandf to /root/udisk and copied $LOGFILE file to /root/udisk/loot/log.txt"
+		logger -t "[PAYLOAD]" "mount /dev/nandf to /root/udisk and copied $LOGFILE file to /root/udisk/loot/log.txt"
+		sync; sleep 1; sync
 		umount /dev/nandf
 	fi
-	sync; sleep 1; sync
+	
 }
 export -f STOP_DEBUG
 
 function LOG() {
-	logger -t PAYLOAD "$1"
+	logger -t "[PAYLOAD]" "$1"
 }
 export -f LOG
